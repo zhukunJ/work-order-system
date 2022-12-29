2df5f7f42b5e839e7403b2c941ec0b26e9a9ee86
devops-kkb <jytian01@kaikeba.com>
2022-12-29 21:10:54 +0800

    del web

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
-if [[ ! -f /opt/workflow/ferry/config/settings.yml ]]
-then
-    cp /opt/workflow/ferry/default_config/* /opt/workflow/ferry/config/
-fi
 if [[ -f /opt/workflow/ferry/config/needinit ]]
 then
     /opt/workflow/ferry/ferry init -c=/opt/workflow/ferry/config/settings.yml
